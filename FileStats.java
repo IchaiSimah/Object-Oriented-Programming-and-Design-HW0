af78fefb2173a4b4af2c865238baba1e0ef46e30
ichaiSimah <ichaisimah@gmail.com>
2025-04-28 17:20:52 +0300

    yes

--- FileStats.java
+++ FileStats.java
@@ -1,43 +1,59 @@
-import java.io.*; 
+import java.io.*;
 import java.util.StringTokenizer;
 
 public class FileStats {
-    public static void printFileStats(FileReader in)throws IOException {
+    public static void printFileStats(FileReader in) throws IOException {
 
-        //count number of numbers in the file
-            
+        // count number of numbers in the file
+
         int totalNumbers = 0;
         int totalSum = 0;
         int positiveCount = 0;
         int negativeCount = 0;
         int zeroCount = 0;
 
-        try (BufferedReader reader = new BufferedReader(new FileReader(fileName))) {
+        try (BufferedReader reader = new BufferedReader(in)) {
+            int sign = 1;
             String line;
             int lineNumber = 1;
+
             while ((line = reader.readLine()) != null) {
-                StringTokenizer tokens = new StringTokenizer(line, ";{}[]=-+_!@#$%^&*():',.");
+                StringTokenizer tokens = new StringTokenizer(line, ";{}[]=+_!@#$%^&*():',.");
                 int lineCount = 0;
                 int lineSum = 0;
-                while (tokens.nextToken() != null) {
+
+                while (tokens.hasMoreTokens()) {
                     String token = tokens.nextToken();
-                    try {
-                        int number = Integer.parseInt(token);
-                        lineCount++;
-                        totalNumbers++;
-                        lineSum += number;
-                        totalSum += number;
-                        if (number > 0) {
-                            positiveCount++;
-                        } else if (number < 0) {
-                            negativeCount++;
+                    StringTokenizer negativeSpliterTokens = new StringTokenizer(token, "-");
+
+                    while (negativeSpliterTokens.hasMoreTokens()) {
+                        String insideToken = negativeSpliterTokens.nextToken();
+
+                        if (insideToken == "") {
+                            sign = -1;
                         } else {
-                            zeroCount++;
+                            try {
+                                int number = sign * Integer.parseInt(insideToken);
+                                lineCount++;
+                                totalNumbers++;
+                                lineSum += number;
+                                totalSum += number;
+                                if (number > 0) {
+                                    positiveCount++;
+                                } else if (number < 0) {
+                                    negativeCount++;
+                                } else {
+                                    zeroCount++;
+                                }
+                                sign = 1;
+                            } catch (NumberFormatException e) {
+                                // Ignore non-integer tokens
+                            }
+
                         }
-                    } catch (NumberFormatException e) {
-                        // Ignore non-integer tokens
                     }
                 }
+
                 double lineAverage = (double) lineSum / lineCount;
                 System.out.printf("Line %d - Average: %.2f\n", lineNumber, lineAverage);
                 lineNumber++;
@@ -47,14 +63,10 @@ public class FileStats {
             System.out.println("Positive numbers: " + positiveCount);
             System.out.println("Negative numbers: " + negativeCount);
             System.out.println("Zeroes: " + zeroCount);
-        }
-        catch (IOException e) {
+        } catch (IOException e) {
             System.out.println("Error reading file: " + e.getMessage());
         }
     }
-
-                        
-        
 
     public static void main(String[] args) throws IOException {
         if (args.length != 1) {
@@ -63,7 +75,7 @@ public class FileStats {
         }
 
         String filename = args[0];
-        Reader in = new FileReader(filename);
+        FileReader in = new FileReader(filename);
 
         if (!(in.ready())) {
             System.out.println("File not found or empty.");
@@ -72,4 +84,4 @@ public class FileStats {
         printFileStats(in);
     }
 
-    }
+}
